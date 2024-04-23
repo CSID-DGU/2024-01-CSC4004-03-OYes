374f0a3b7ab0d3eb6aaed2d8769ca6859060e067
rlatkdgus2627 <rlatkdgus2627@gmail.com>
2024-04-23 23:42:46 +0900

    feat: VoiceGenerator 클래스 생성
    - 기존의 TTS 생성 파일을 이용하여 VoiceGenerator 클래스로 변환
    - main으로 실행을 했을 때 테스트 코드가 동작하도록 작성

--- VoiceGenerator.py
+++ VoiceGenerator.py
@@ -1,13 +1,33 @@
 import torch
+import os
 from TTS.api import TTS
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device = device)
+class VoiceGenerator:
+    def __init__(self) -> None:
+        # loading xtts_v2 tts model
+        self.tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device = device)
+        self.input_str = ""
+    def set_correction_script(self, input_str) -> None:
+        self.input_str = input_str
+    def get_basic_voice(self) -> str:
+        output_path = "output.wav"
+        if self.input_str == "":
+            # do something
+            print("Error : empty input string")
+            os.abort() # SIGABRT
+        # generate speech by cloning a voice using default settings
+        self.tts.tts_to_file(
+            text=self.input_str,
+            speaker_wav= "speaker_voice.wav",
+            file_path=output_path,
+            language="ko")
+        
+        return os.path.abspath(output_path)
 
-# generate speech by cloning a voice using default settings
+if __name__ == "__main__":
+    vg = VoiceGenerator()
+    vg.set_correction_script("안녕하세요. 반갑습니다.")
+    print(vg.get_basic_voice())
 
-text = '''
-여름 장이란 애시당초에 글러서, 해는 아직 중천에 있건만 장판은 벌써 쓸쓸하고 더운 햇발이 벌여놓은 전 휘장 밑으로 등줄기를 훅훅 볶는다.마을 사람들은 거지반 돌아간 뒤요, 팔리지 못한 나뭇군패가 길거리에 궁싯거리고들 있으나 석윳병이나 받고 고깃마리나 사면 족할 이 축들을 바라고 언제까지든지 버티고 있을 법은 없다. 춥춥스럽게 날아드는 파리떼도 장난군 각다귀들도 귀치않다. 얽둑배기요 왼손잡이인 드팀전의 허생원은 기어코 동업의 조선달에게 낚아보았다.
-'''
-tts.tts_to_file(text=text, speaker_wav= "speaker_voice.wav", file_path="output.wav", language="ko")
