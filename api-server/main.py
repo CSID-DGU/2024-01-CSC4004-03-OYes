11a7fef3ebc5d14602cae85793fca6ca7d452e42
Kim Haehwan <24457185+khaehwan@users.noreply.github.com>
2024-06-09 03:09:50 +0900

    feat: API서버 WORK_DIR 생성 및 임시파일 삭제 기능 추가
    - API서버 WORK_DIR가 없을 시 자동 생성 기능 추가
    - 작동 중 생성된 임시파일 자동 삭제 기능 추가
    - 미들웨어 설정 부분 추가
    
    Signed-off-by: Kim Haehwan <24457185+khaehwan@users.noreply.github.com>

--- api-server/main.py
+++ api-server/main.py
@@ -7,9 +7,21 @@ from tempfile import NamedTemporaryFile
 from typing import IO
 
 from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # React 앱이 실행되는 도메인
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # STT 객체 생성
 from speechcorrection import stt
@@ -28,10 +40,15 @@ correction.api_key = OPENAI_API_KEY
 # TTS 객체 생성
 from speechcorrection import tts
 
-WORK_DIR = "test/demo-file/"
-ABS_WORK_DIR = os.path.abspath(WORK_DIR)
-
 tts = tts.VoiceGenerator()
+
+WORK_DIR = "temp-voice/"
+ABS_WORK_DIR = os.path.abspath(WORK_DIR)
+try:
+    if not os.path.exists(ABS_WORK_DIR):
+        os.makedirs(ABS_WORK_DIR)
+except OSError:
+    print(f"{ABS_WORK_DIR} 경로를 찾을 수 없습니다.")
 
 # 프로그램 작동 로그
 execution_log: str = str()
@@ -61,8 +78,8 @@ async def speech_correction(model: str = Form(...), rvc_enabled: str = Form(...)
     # STT 모듈 동작
     total_start_time = time.time()
     start_time = total_start_time
-    path = await save_file(file.file)
-    origin_script = execute_stt(path)
+    origin_file_path = await save_file(file.file)
+    origin_script = execute_stt(origin_file_path)
     end_time = time.time()
     elapsed_time = end_time - start_time
 
@@ -124,6 +141,10 @@ async def speech_correction(model: str = Form(...), rvc_enabled: str = Form(...)
     execution_log += current_log
     print(current_log)
 
+    # 임시 파일 삭제
+    if os.path.isfile(origin_file_path):
+        os.remove(origin_file_path)
+
     return FileResponse(changed_voice_path, filename=f"c_{model_name}_{file_name_suffix}.wav",
                         media_type="audio/wav")
 
@@ -132,8 +153,8 @@ async def speech_correction(model: str = Form(...), rvc_enabled: str = Form(...)
 async def speech_to_text(file: UploadFile = File(...)):
     global execution_log
     start_time = time.time()
-    path = await save_file(file.file)
-    origin_script = execute_stt(path)
+    origin_file_path = await save_file(file.file)
+    origin_script = execute_stt(origin_file_path)
     end_time = time.time()
     elapsed_time = end_time - start_time
 
@@ -142,6 +163,10 @@ async def speech_to_text(file: UploadFile = File(...)):
     current_log += f"소요 시간: {elapsed_time:.6f} sec\n\n"
     execution_log = current_log
     print(current_log)
+
+    # 임시 파일 삭제
+    if os.path.isfile(origin_file_path):
+        os.remove(origin_file_path)
 
     return origin_script
 
@@ -204,6 +229,10 @@ async def voice_infer(model: str = Form(...), file: UploadFile = File(...)):
     current_log += f"소요 시간: {elapsed_time:.6f} sec\n\n"
     execution_log = current_log
     print(current_log)
+
+    # 임시 파일 삭제
+    if os.path.isfile(basic_voice_path):
+        os.remove(basic_voice_path)
 
     return FileResponse(changed_voice_path, filename=f"infer_{model_name}_{file_name_suffix}.wav",
                         media_type="audio/wav")
